ae626ad017f2c46a4d155dc1a421be49acae5efe
Виталий Насретдинов <nasretdinovs@yandex.ru>
2022-05-15 01:07:07 +0300

    Deploy. Change index. Add telegram

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Привет Альфие!')
+    return HttpResponse('У меня получилось! Привет Альфие! Ура!')
 
 
 def second_page(request):
